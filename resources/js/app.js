bb54bdce347a03de55a15a59c22f17335f2db187
Casper Kizewski <casperkizewski@outlook.com>
2024-08-12 19:00:53 +0200

    Project view change

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     }, {
         threshold: 0.1,
-        rootMargin: '-100px 0px'
+        rootMargin: '-50px 0px'
     });
 
     document.querySelectorAll('[data-project-card]').forEach(card => {
