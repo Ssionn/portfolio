f8e0e53145ac6ab4d0a893fc353831dad1a87e33
Ssionn <casperkizewski@outlook.com>
2024-09-02 22:30:06 +0200

    Language files for static text

---
+++ lang/en/portfolio.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'title' => "SsionnHub",
+
+    'introduction' => "Hi, My name is Casper",
+    'description' => "Passionate PHP developer specializing in Laravel and Symfony. I craft elegant solutions to complex problems, always eager to learn and apply new technologies. Gaming enthusiast and tech hobbyist, I'm on a mission to create intuitive digital experiences that push boundaries.",
+
+    'projects' => [
+        'header' => 'Projects',
+
+        'data' => [
+            'contributors' => ":contributor_count Contributors",
+            'commits' => ":commit_count Commits",
+            'stars' => ":star_count Stars",
+            'forks' => ":fork_count Forks"
+        ],
+    ],
+
+    'copyright' => ":date Casper Kizewski. All rights reserved.",
+
+    'links' => [
+        'github' => 'GitHub',
+        'license' => 'MIT License',
+    ],
+];
--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -15,7 +15,7 @@
         <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('android-chrome-192x192.png') }}">
         <link rel="icon" type="image/png" sizes="512x512" href="{{ asset('android-chrome-512x512.png') }}">
 
-        <title>Portfolio</title>
+        <title>{{ __('portfolio.title') }}</title>
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
@@ -57,17 +57,17 @@
                     </div>
                     <div class="w-full md:w-1/2 text-center md:text-left mt-6 md:mt-0">
                         <h1 class="text-5xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-[#FFD54F] to-[#FFA000]">
-                            Hi, My name is Casper
+                            {{ __('portfolio.introduction') }}
                         </h1>
                         <p class="text-xl text-white">
-                            Passionate PHP developer specializing in Laravel and Symfony. I craft elegant solutions to complex problems, always eager to learn and apply new technologies. Gaming enthusiast and tech hobbyist, I'm on a mission to create intuitive digital experiences that push boundaries.
+                            {{ __('portfolio.description') }}
                         </p>
                     </div>
                 </div>
             </div>
 
             <div class="container mx-auto max-w-6xl p-6 sm:p-6 lg:p-8 mt-20">
-                <h2 class="text-4xl font-bold text-white mb-8 text-center">Projects</h2>
+                <h2 class="text-4xl font-bold text-white mb-8 text-center">{{ __('portfolio.projects.header') }}</h2>
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                     @foreach($projects as $project)
                         <a href="https://github.com/{{ $project->owner }}/{{ $project->repo }}" target="_blank" class="flex flex-col bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 opacity-0 translate-y-4 transition-all duration-700 ease-out h-full" data-project-card>
@@ -82,19 +82,19 @@
                                 <div class="flex flex-wrap items-center text-sm text-gray-500 gap-4 mt-auto pt-4 border-t border-gray-200">
                                     <span class="flex items-center">
                                         <svg class="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"></path></svg>
-                                        <span>{{ $project->contributor_count ?? 0 }} Contributors</span>
+                                        <span>{{ __('portfolio.projects.data.contributors', ['contributor_count' => $project->contributor_count ?? 0]) }}</span>
                                     </span>
                                     <span class="flex items-center">
                                         <svg class="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd"></path></svg>
-                                        <span>{{ $project->commit_count ?? 0 }} Commits</span>
+                                        <span>{{ __('portfolio.projects.data.commits', ['commit_count' => $project->commit_count ?? 0]) }}</span>
                                     </span>
                                     <span class="flex items-center">
                                         <svg class="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd"></path></svg>
-                                        <span>{{ $project->star_count ?? 0 }} Stars</span>
+                                        <span>{{ __('portfolio.projects.data.stars', ['star_count' => $project->star_count ?? 0]) }}</span>
                                     </span>
                                     <span class="flex items-center">
                                         <svg class="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z" clip-rule="evenodd"></path></svg>
-                                        <span>{{ $project->fork_count ?? 0 }} Forks</span>
+                                        <span>{{ __('portfolio.projects.data.forks', ['fork_count' => $project->fork_count ?? 0]) }}</span>
                                     </span>
                                 </div>
                             </div>
@@ -108,19 +108,19 @@
             <div class="container mx-auto px-4">
                 <div class="flex flex-col md:flex-row justify-between items-center text-sm">
                     <div class="mb-2 md:mb-0">
-                        <p>&copy; {{ date('Y') }} Casper Kizewski. All rights reserved.</p>
+                        <p>&copy; {{ __('portfolio.copyright', ['date' => date('Y')]) }}</p>
                     </div>
                     <div class="flex items-center space-x-4">
                         <p>Licensed under
                             <a href="https://github.com/ssionn/portfolio/blob/production/LICENSE"
                                class="text-blue-300 hover:text-blue-100 underline"
                                target="_blank"
-                               rel="noopener noreferrer">MIT License</a>
+                               rel="noopener noreferrer">{{ __('portfolio.links.license') }}</a>
                         </p>
                         <a href="https://github.com/ssionn/portfolio"
                            class="text-blue-300 hover:text-blue-100 underline"
                            target="_blank"
-                           rel="noopener noreferrer">GitHub</a>
+                           rel="noopener noreferrer">{{ __('portfolio.links.github') }}</a>
                     </div>
                 </div>
             </div>
